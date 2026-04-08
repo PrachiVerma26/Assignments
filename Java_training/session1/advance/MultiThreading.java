100fd988051457db4bf1b390c6d8924b44016ab5
PrachiVerma26 <prachiverma26602@gmail.com>
2026-04-08 22:16:21 +0530

    08-04-2026-16-java_training: added multi-threading example program

---
+++ Java_training/session1/advance/MultiThreading.java
@@ -0,0 +1,45 @@
+package session1.advance;
+
+//multi-threading example
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// Task representing an order
+class OrderTask implements Runnable {
+    private int orderId;
+
+    public OrderTask(int orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +
+                " processing Order #" + orderId);
+        try {
+            Thread.sleep(1000); // simulate processing time
+        } catch (InterruptedException e) {
+            System.out.println("Error processing order " + orderId);
+        }
+
+        System.out.println(Thread.currentThread().getName() +
+                " completed Order #" + orderId);
+    }
+}
+
+public class MultiThreading{
+
+    public static void main(String[] args) {
+        // Create a pool with 3 threads
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        // Submit 6 tasks
+        for (int i = 1; i <= 4; i++) {
+            executor.execute(new OrderTask(i));
+        }
+
+        // Shutdown pool
+        executor.shutdown();
+        System.out.println("All tasks submitted...");
+    }
+}
