205d8d46527aa73e9a1fc6b83db80b415e255473
PrachiVerma26 <prachiverma26602@gmail.com>
2026-04-08 21:14:21 +0530

    08-04-2026-15-java_training: added file operation added

--- Java_training/session1/advance/FileOperation.java
+++ Java_training/session1/advance/FileOperation.java
@@ -1,4 +1,30 @@
 package session1.advance;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+//simple file I/O operation to read data from a text file.
 public class FileOperation {
+    public static void main(String[] args) {
+        String filePath = "Java_training/session1/advance/test.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            System.out.println("Reading file content:\n");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error: File not found! Please check the file path.");
+        }
+        catch (IOException e) {
+            System.out.println("Error: Problem occurred while reading the file.");
+
+        }
+        finally {
+            System.out.println("File operation completed.");
+        }
+    }
 }
