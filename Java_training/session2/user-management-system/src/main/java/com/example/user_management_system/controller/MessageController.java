aa36965b1a1a21fa3f0b8345445e1bad47a3a604
PrachiVerma26 <prachiverma26602@gmail.com>
2026-04-11 19:21:19 +0530

    11-04-2026-12-java_training/session2: implemented message api endpoint

--- Java_training/session2/user-management-system/src/main/java/com/example/user_management_system/controller/MessageController.java
+++ Java_training/session2/user-management-system/src/main/java/com/example/user_management_system/controller/MessageController.java
@@ -1,4 +1,23 @@
 package com.example.user_management_system.controller;
 
+import com.example.user_management_system.service.MessageService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
 public class MessageController {
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService=messageService;
+    }
+
+    @GetMapping
+    public String getMessage(@RequestParam String type){
+        return messageService.getMessage(type);
+    }
 }
--- Java_training/session2/user-management-system/src/main/java/com/example/user_management_system/service/MessageService.java
+++ Java_training/session2/user-management-system/src/main/java/com/example/user_management_system/service/MessageService.java
@@ -1,4 +1,53 @@
 package com.example.user_management_system.service;
 
+import com.example.user_management_system.component.formatter.MessageFormatter;
+import com.example.user_management_system.component.formatter.MessageType;
+import com.example.user_management_system.exception.InvalidMessageTypeException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class MessageService {
+
+    private final Map<MessageType, MessageFormatter> formatterMap;
+
+    public MessageService(List<MessageFormatter> formatters) {
+        this.formatterMap = formatters.stream()
+                .collect(Collectors.toMap(
+                        MessageFormatter::getType,
+                        formatter -> formatter
+                ));
+    }
+
+    // return formatted message based on the input type.
+    public String getMessage(String type) {
+
+        //validation for checking message is not null
+        if(type==null){
+            throw new InvalidMessageTypeException("Message type cannot be null.");
+        }
+
+        // empty check
+        if(type.trim().isEmpty()){
+            throw new InvalidMessageTypeException("Message type cannot be empty.");
+        }
+        //normalize input
+        String normalizedType=type.trim().toUpperCase();
+        MessageType messageType;
+        try {
+            messageType = MessageType.valueOf(normalizedType);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidMessageTypeException("Invalid message type: " + type);
+        }
+
+        MessageFormatter formatter = formatterMap.get(messageType);
+
+        if (formatter == null) {
+            throw new InvalidMessageTypeException("Formatter not found for type: " + type);
+        }
+        return formatter.formatMessage();
+    }
 }
