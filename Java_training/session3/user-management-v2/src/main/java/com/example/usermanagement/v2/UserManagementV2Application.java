4b5851518489d9a903c0a672157354dd64c2af24
PrachiVerma26 <prachiverma26602@gmail.com>
2026-04-15 13:57:09 +0530

    15-04-2026-01-java_training/session3: Initial project structure and readme file

---
+++ Java_training/session3/user-management-v2/src/main/java/com/example/usermanagement/v2/UserManagementV2Application.java
@@ -0,0 +1,13 @@
+package com.example.usermanagement.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementV2Application.class, args);
+	}
+
+}
