43189aa0eaa3e66a198282429c24b8d3ff5281d3
PrachiVerma26 <prachiverma26602@gmail.com>
2026-04-15 17:23:56 +0530

    15-04-2026-02-java_training/session3: Added user entity

--- Java_training/session3/user-management-v2/src/main/java/model/User.java
+++ Java_training/session3/user-management-v2/src/main/java/model/User.java
@@ -1,4 +1,48 @@
 package model;
 
 public class User {
+    private Long id;
+    private String name;
+    private int age;
+    private String role;
+
+    //default-constructor
+    User(){
+    }
+
+    //all-arguments-constructor
+    User(Long id, String name, int age, String role){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.role=role;
+    }
+
+    //getters
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getRole() {
+        return role;
+    }
+
+    //setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
