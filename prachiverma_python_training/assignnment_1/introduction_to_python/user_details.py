ca8d966e1ffaf944d19f3d96c8d803bb3d9e8312
PrachiVerma26 <prachiverma26602@gmail.com>
2026-06-10 19:03:28 +0530

    feat: add user details program to introduction to python

---
+++ prachiverma_python_training/assignnment_1/introduction_to_python/user_details.py
@@ -0,0 +1,8 @@
+""" program to display user details """
+
+#input from user
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+
+# formatted message to display user details
+print(f"Hello, {name}. Your age is {age}.")
