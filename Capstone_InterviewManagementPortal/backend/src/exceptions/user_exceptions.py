8ef4bf0e6242a5747d673aa1c45479489201e15a
PrachiVerma26 <prachiverma26602@gmail.com>
2026-06-29 19:45:26 +0530

    feat: add enable user functionality

--- Capstone_InterviewManagementPortal/backend/src/exceptions/user_exceptions.py
+++ Capstone_InterviewManagementPortal/backend/src/exceptions/user_exceptions.py
@@ -8,13 +8,6 @@ class UserAlreadyInactiveException(Exception):
     """ Raised when attempting to disable an already inactive user."""
     pass
 
-class UserAlreadyActiveException(Exception):
-    """ Raised when attempting to reactivate an already active user."""
-    pass
-
 class InvalidObjectIdException(Exception):
     """ Raised when the supplied MongoDB ObjectId is invalid."""
-    pass
-class UserNotFoundException(Exception):
-    """ Raised when a user is not found."""
     pass
